a3e4b12709cb0669f68443bac4eb0ea30e0cd5bf
Stellarlap <100299314+Stellarlap@users.noreply.github.com>
2022-03-09 11:11:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    if (player && player_2 > 0) {
+        basic.showIcon(IconNames.Skull)
+    } else if (false) {
+        basic.showIcon(IconNames.Square)
+    }
+})
+let player_2 = 0
+let player = 0
+player = 5
+player_2 = 6
+basic.forever(function () {
+    if (0 == 0) {
+        player += 1
+    }
+    if (0 == 0) {
+        player += 1
+    }
+    if (0 == 0) {
+        player_2 += 1
+    }
+})
